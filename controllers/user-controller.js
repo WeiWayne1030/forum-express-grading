0c330f27c59c3624f937db8bd7d6ac555fea1c7a
WeiWayne1030 <wayne10032004@gmail.com>
2023-05-28 17:25:03 +0800

    fix: can not render profilePage problem

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -52,12 +52,7 @@ const userController = {
     ])
       .then(([user, comments]) => {
         if (!user) throw new Error('User not found!')
-        if (user.id === req.params.id) {
-          res.render('users/profile', { user, comments })
-        } else {
-          req.flash('error_messages', 'Do not have authentication to view other user page!')
-          res.redirect(`/users/${req.user.id}`)
-        }
+        res.render('users/profile', { user, comments })
       })
       .catch(err => next(err))
   },
